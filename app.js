5670dbc56e73f8edf366c19bbe1b3d2bb0215faa
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-09-16 22:42:32 -0400

    updated app.js and index.html

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-//user input to search for city
-//make sure we have weather api
-
-//use api to update html with city/weather info
-//use api to update html with 5day forecast
-
-//make a list of cities for history 
-
 const apiKey = "&appid=84d5d594c8842f1a8ba61cc7db4edf08";
 var forecastUrl = "http://api.openweathermap.org/data/2.5/weather?q="
 var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q="
-var uvURL = "http://api.openweathermap.org/data/2.5/onecall?lat="
+
 
 $(document).ready(function () {
 
@@ -19,8 +11,8 @@ $(document).ready(function () {
 
         console.log(userInput);
         searchWeather(userInput);
-        getForcast(userInput);
-        currentUv(userInput);
+        //getForcast(userInput);
+
     })
 
     //make a current weather ajax call
@@ -29,48 +21,25 @@ $(document).ready(function () {
             type: "GET",
             url: forecastUrl + city + apiKey
         }).then(function (data) {
-            console.log(data);
-            name
-            console.log(data.name);
-            //date
-            temp
-            console.log(data.main.temp);
-            humidity
-            console.log(data.main.humidity);
-            windSpeed
-            console.log(data.wind.speed);
-            uvIndex
-            console.log(data.main.temp);
+
+            var displayInfo = `<p class="city">City: ${data.name}</p>
+            <p>Temp: ${data.main.temp}</p> <p>humidity: ${data.main.humidity}</p> <p>wind speed: ${data.wind.speed}</p>`;
+            $("#current-weather-data").html(displayInfo);
+            uvIndex(data.coord.lat, data.coord.lon);
         })
     }
 
-    function getForcast(city) {
-        $.ajax({
-            type: "GET",
-            url: fiveDayUrl + city + apiKey
-        }).then(function (data) {
-            console.log(data);
-        })
-    }
+    function example() {
 
-    function currentUv(city) {
+    };
+
+    function uvIndex(lat, lon) {
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon;
         $.ajax({
             type: "GET",
             url: uvURL + apiKey
         }).then(function (data) {
-            var cityLatitude = data.coord.lat;
-            var cityLongitude = data.coord.lon;
-            $("#forecast").text("5-day Forecast");
-            currentUVLabel = $("<span>").text("UV Index: ");
-            currentUVBadge = $("<span>").text(data.value);
-            console.log(data.value);
-            console.log(cityLatitude, cityLongitude);
+            console.log(data);
         })
     }
-
-
-
-
-
-
 })
